7bc69ca0545fb2b3fce8bddd6079fa9e10f2f014
Jsegel <segeljosh@gmail.com>
2019-12-19 18:23:26 -0600

    making placeholder array for questions and changing basic html structure

--- questions.js
+++ questions.js
@@ -1,1 +1,13 @@
-console.log("hello")
+var questions = [
+    {
+        title: "Question 1",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "Choice 1"
+    },
+    {
+        title: "Question 2",
+        choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        answer: "Answer 3"
+    },
+    // more questions here
+];
